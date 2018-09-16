74327e05c7350fd704abbde05e8266cec7cf4e33
bsypher <ben.sypher@gmail.com>
2018-09-16 15:01:45 -0400

    Added quiz questions to js file

--- main.js
+++ main.js
@@ -11,3 +11,44 @@ buildQuiz();
 
 // on submit, show results
 submitButton.addEventListener('click', showResults);
+
+const myQuestions = [
+  {
+    question: "What is the capital of Italy",
+    answers: {
+      a: "Rome",
+      b: "Spain",
+      c: "Naples"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "What is the capital of Germany?",
+    answers: {
+      a: "Berlin",
+      b: "Oslo",
+      c: "London"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Who founded Turkey",
+    answers: {
+      a: "Mesut Ozil",
+      b: "Mustafa Kemal",
+      c: "Ozguhan Mehmetoglou",
+      d: "John Stamos"
+    },
+    correctAnswer: "b"
+  }
+  {
+    question: "Who won the Battle of Midway",
+    answers: {
+      a: "Japan",
+      b: "Taiwan",
+      c: "Canada",
+      d: "United States"
+    },
+    correctAnswer: "d"
+  }
+];
