27ac5483613f4f5b8f98bc454cd0e2533489d7e9
bsypher <ben.sypher@gmail.com>
2018-09-16 22:32:14 -0400

    added more questions

--- main.js
+++ main.js
@@ -1,9 +1,105 @@
-const previousButton = document.getElementById("previous");
-const nextButton = document.getElementById("next");
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
-
 (function() {
+  const myQuestions = [
+    {
+      question: "Who was the last Tsar of Russia?",
+      answers: {
+        a: "Ivan IV",
+        b: "Peter the Great",
+        c: "Nicholas II"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Who founded Turkey?",
+      answers: {
+        a: "Mehmed II",
+        b: "Burak Yilmaz Pasha",
+        c: "Mustafa Kemal"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which of the following was not a Canadian Prime Minister?",
+      answers: {
+        a: "Joe Clark",
+        b: "John Turner",
+        c: "John Diefenbaker",
+        d: "Stubby Clapp"
+      },
+      correctAnswer: "d"
+    },
+    {
+    question: "Who won the battle of Midway?",
+      answers: {
+        a: "Japan",
+        b: "Australia",
+        c: "United States",
+        d: "Thailand"
+      },
+      correctAnswer: "c"
+    },
+    {
+    question: "What is the present day name of Stalingrad?",
+      answers: {
+        a: "Astana",
+        b: "Volgogrod",
+        c: "Rostov",
+        d: "Donestk"
+      },
+      correctAnswer: "b"
+    },
+    {
+    question: "Finland defeated this country during the Winter War?",
+      answers: {
+        a: "Sweden",
+        b: "Soviet Union",
+        c: "Sweden",
+        d: "Germany"
+      },
+      correctAnswer: "b"
+    },
+    {
+    question: "Who won the battle of Midway?",
+      answers: {
+        a: "Japan",
+        b: "Australia",
+        c: "United States",
+        d: "Thailand"
+      },
+      correctAnswer: "c"
+    },
+    {
+    question: "Who won the battle of Midway?",
+      answers: {
+        a: "Japan",
+        b: "Australia",
+        c: "United States",
+        d: "Thailand"
+      },
+      correctAnswer: "c"
+    },
+    {
+    question: "Japan once included part of which island?",
+      answers: {
+        a: "Sakhalin",
+        b: "Australia",
+        c: "New Zealand",
+        d: "Yurkazi"
+      },
+      correctAnswer: "a"
+    },
+    {
+    question: "A pope has not come from which country?",
+      answers: {
+        a: "Poland",
+        b: "Russia",
+        c: "Italy",
+        d: "Germany"
+      },
+      correctAnswer: "c"
+    },
+  ];
+
   function buildQuiz() {
     // we'll need a place to store the HTML output
     const output = [];
@@ -18,17 +114,19 @@ let currentSlide = 0;
         // ...add an HTML radio button
         answers.push(
           `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
+             <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+           </label>`
         );
       }
 
       // add this question and its answers to the output
       output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
+        `<div class="slide">
+           <div class="question"> ${currentQuestion.question} </div>
+           <div class="answers"> ${answers.join("")} </div>
+         </div>`
       );
     });
 
@@ -68,35 +166,50 @@ let currentSlide = 0;
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
   }
 
+  function showSlide(n) {
+    slides[currentSlide].classList.remove("active-slide");
+    slides[n].classList.add("active-slide");
+    currentSlide = n;
+    
+    if (currentSlide === 0) {
+      previousButton.style.display = "none";
+    } else {
+      previousButton.style.display = "inline-block";
+    }
+    
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = "none";
+      submitButton.style.display = "inline-block";
+    } else {
+      nextButton.style.display = "inline-block";
+      submitButton.style.display = "none";
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
   const quizContainer = document.getElementById("quiz");
   const resultsContainer = document.getElementById("results");
   const submitButton = document.getElementById("submit");
-  const myQuestions = [
-    
 
   // display quiz right away
   buildQuiz();
 
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  showSlide(0);
+
   // on submit, show results
   submitButton.addEventListener("click", showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
 })();
-function showSlide(n) {
-  slides[currentSlide].classList.remove('active-slide');
-  slides[n].classList.add('active-slide');
-  currentSlide = n;
-  if(currentSlide===0){
-    previousButton.style.display = 'none';
-  }
-  else{
-    previousButton.style.display = 'inline-block';
-  }
-  if(currentSlide===slides.length-1){
-    nextButton.style.display = 'none';
-    submitButton.style.display = 'inline-block';
-  }
-  else{
-    nextButton.style.display = 'inline-block';
-    submitButton.style.display = 'none';
-  }
-}
-showSlide(0);
